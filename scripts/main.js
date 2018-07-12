6ea7193c22998fd678e7c7e7cf02ce14de0b30f7
Chintan Patel <chintel@outlook.in>
2018-07-13 05:28:53 +0530

    Final Project

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,16 @@
+$(function() {
+    $('#wmtoggler').change(function() {
+        $('#ss-watermark').toggle(this.checked);
+    }).change(); //ensure visible state matches initially    
+});
 
-$(function () {
-  $('#wmtoggler').change(function () {        
-     $('#ss-watermark').toggle(this.checked);
-  }).change(); //ensure visible state matches initially    
-});  
-  
-$(function () {
-  $('#textToggler').change(function () {       
-     $('.textmark').toggle(this.checked);
-  }).change(); //ensure visible state matches initially    
+$(function() {
+    $('#textToggler').change(function() {
+        $('.textmark').toggle(this.checked);
+    }).change(); //ensure visible state matches initially    
+});
+
+$("#preview").on("click", function() {
+    $(this).toggleClass("on");
+    $("#ssimg").toggle();
 });
